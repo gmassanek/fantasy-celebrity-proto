fdb74d7764ef903833a56a3031a8ea3d558e6706
Geoff Massanek <geoff@groupon.com>
2014-12-24 19:28:25 -0500

    uniq

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -2,6 +2,6 @@ module TeamsHelper
   def available_positions(team, player)
     team.league.available_positions.select do |position|
       position == player.position || position == Position.flex || position == Position.bench
-    end
+    end.uniq
   end
 end
